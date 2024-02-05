f683608cc1fd63262c4cb97a755316ad0982fcab
João <joaosaraivasapori@gmail.com>
2024-02-05 10:14:35 -0300

    Exercício 3 da aula II

--- Módulo 2 - Lógica de Programação I/Aula II/Exercico 3/index.js
+++ Módulo 2 - Lógica de Programação I/Aula II/Exercico 3/index.js
@@ -4,3 +4,33 @@
     O jogo deve dar dicas ao jogador, informando se o número é maior ou menor do que a tentativa atual. 
     O jogador deve continuar a adivinhar até acertar o número. No final, deverá informar a quantidade de tentativas.
 */
+
+
+const number = Math.floor(Math.random() *100)
+const prompt = require('prompt-sync')();
+let tryies = 0
+
+console.log(number);
+
+
+while(entry!==number){
+
+    let entry = parseInt(prompt('Chute um número entre 0 e 100.: '));
+    if(entry===number){
+        console.log(`Você acertou com ${tryies} tentativas(a). O número era ${number}`);
+    }
+    else if(entry < number){
+        tryies++
+        console.log("Você errou, chutou a baixo do valor");
+        let entry = parseInt(prompt('Chute um número entre 0 e 100.: '));
+
+    }
+    else if(entry> number){
+        tryies++
+        console.log("Você errou, chutou a cima do valor");
+        let entry = parseInt(prompt('Chute um número entre 0 e 100.: '));
+
+    }else{
+        console.log("Entre somente com números de 0 a 100")
+    }
+}
