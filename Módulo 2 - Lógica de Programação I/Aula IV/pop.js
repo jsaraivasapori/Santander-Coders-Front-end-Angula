95d35b459cbc1dbffcf57f50e44ace07ef1ee885
João Vitor <joaosaraivasapori@gmail.com>
2024-02-12 14:04:22 -0300

    Exercício pop

---
+++ Módulo 2 - Lógica de Programação I/Aula IV/pop.js
@@ -0,0 +1,6 @@
+const numeros = [1, 2, 3, 4]
+
+const primeiroElementoRemovido = numeros.pop()
+console.log(`Tamanho do array após primeira modificação: ${numeros.length}`);
+const  segundoElementoRemovido = numeros.pop()
+console.log(`Tamanho do array após segunda modificação: ${numeros.length}`)
