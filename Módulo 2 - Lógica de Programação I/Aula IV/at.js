28def08b95a2603a49faeac40aed2b063481b0a0
João Vitor <joaosaraivasapori@gmail.com>
2024-02-12 14:19:33 -0300

    Exercício at

---
+++ Módulo 2 - Lógica de Programação I/Aula IV/at.js
@@ -0,0 +1,10 @@
+const produtos = ["sabao", "detergente", "amaciante", "alvejante", "sapolio"];
+
+const primeiroElemento = produtos.at(0)
+console.log(`Primeiro elemento do array : ${primeiroElemento}`)
+
+const terceiroElemento = produtos.at(2)
+console.log(`Terceiro elemento do array : ${terceiroElemento}`)
+
+const ultimoElemento = produtos.at(-1)
+console.log(`Último elemento do array : ${ultimoElemento}`)
