de9cab763efad91708499db205b8ad7250b1d7c7
João Vitor <joaosaraivasapori@gmail.com>
2024-02-12 14:31:01 -0300

    Exercício concat

---
+++ Módulo 2 - Lógica de Programação I/Aula IV/concat.js
@@ -0,0 +1,11 @@
+const lista1 = ["banana", "pera", "melancia"];
+const lista2 = ["alface", "tomate", "rucula"];
+
+const lista12 = lista1.concat(lista2)
+console.log(lista12)
+
+const lista3 = ["limao", "laranja", "acerola"];
+const lista4 = ["pimenta", "pimentao", "alho"];
+
+const lista34 = lista3.concat(lista4)
+console.log(lista34)
