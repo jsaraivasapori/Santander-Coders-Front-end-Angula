b2b8ba86b3372411d5914997de19a9d761b0ea58
João Vitor <joaosaraivasapori@gmail.com>
2024-02-12 14:50:30 -0300

    Exercício every

---
+++ Módulo 2 - Lógica de Programação I/Aula IV/every.js
@@ -0,0 +1,21 @@
+var pessoas = [
+  { nome: "João", idade: 22 },
+  { nome: "Maria", idade: 17 },
+  { nome: "Pedro", idade: 18 },
+  { nome: "Ana", idade: 22 },
+  { nome: "Carlos", idade: 16 },
+];
+
+console.log(pessoas.every((pessoa) =>{
+  return pessoa.idade >= 18
+ 
+})); 
+
+console.log(pessoas.every((pessoa) =>{
+  pessoa.nome.length > 3
+}));
+
+console.log( pessoas.every((pessoa) =>{
+  return pessoa.nome && pessoa.idade != undefined
+ 
+}));
