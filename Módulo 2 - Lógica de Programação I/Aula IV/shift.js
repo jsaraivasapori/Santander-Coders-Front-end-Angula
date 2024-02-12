03c554104345892f718672d0e75cee3d0bc6d348
João Vitor <joaosaraivasapori@gmail.com>
2024-02-12 14:11:40 -0300

    Exercício shift

---
+++ Módulo 2 - Lógica de Programação I/Aula IV/shift.js
@@ -0,0 +1,5 @@
+const linguagens = ["javascript", "python", "golang", "java"];
+const primeiroElementoRemovido = linguagens.shift()
+console.log(`Tamanho do array após a retirada do elemento '${primeiroElementoRemovido}' :${linguagens.length}`)
+const segundoElemntoRemovido = linguagens.shift()
+console.log(`Tamanho do array após a retirada do elemento '${segundoElemntoRemovido}' :${linguagens.length}`)
