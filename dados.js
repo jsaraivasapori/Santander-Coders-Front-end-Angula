53bcee36f8f5c29a9c548325ff26b5f446de9f58
João Vitor <joaosaraivasapori@gmail.com>
2024-02-19 21:24:53 -0300

    Criaçao do arquivo de dados dos clientes

---
+++ dados.js
@@ -0,0 +1,8 @@
+let dadosContato = [
+  {cliente: "João Vitor Saraiva Sapori", email: "joaosaraiva@dev.com", receberEmail: true},
+  {cliente: "Maria Silva", email: "mariasilva@lla.com", receberEmail: false},
+  {cliente: "Carlos Pereira", email: "carlospereira@delo.com", receberEmail: true},
+  {cliente: "Ana Souza", email: "anasouza@email.com", receberEmail: false},
+  {cliente: "Pedro Costa", email: "pedrocosta@ton.com", receberEmail: true},
+  {cliente: "Julia Santos", email: "juliasantos@alber.com", receberEmail: false}
+]
