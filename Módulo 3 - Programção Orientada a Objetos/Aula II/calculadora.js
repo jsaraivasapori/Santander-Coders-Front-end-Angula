0cb632217c703ccd305a2aabd5121113c52cd021
João Vitor <joaosaraivasapori@gmail.com>
2024-02-29 22:48:50 -0300

    POO - Calculadora básica

---
+++ Módulo 3 - Programção Orientada a Objetos/Aula II/calculadora.js
@@ -0,0 +1,74 @@
+/*
+Vocês deverão implementar uma calculadora simples, contendo as 4 operações básicas;
+
+REQUISITOS:
+
+Quando a calculadora for instanciada, deverá receber 3 argumentos (um número qualquer, uma operação a ser realizada e um segundo número);
+
+Vocês precisam validar se os argumentos recebidos são válidos;
+
+A partir disso, demonstram o uso de sua calculadora através do terminal, executando seu script;
+*/
+
+class Calculo{
+  PrimeiroNumero;
+  SegundoNumero;
+
+  
+  constructor(primeiroNumero,segundoNumero){
+
+   
+   if(typeof primeiroNumero !== "number" || typeof segundoNumero !== "number" ){
+      throw new Error("você precisa passar numeros nas operações")
+    }
+
+    this.PrimeiroNumero = primeiroNumero
+    this.SegundoNumero = segundoNumero
+  
+    /*switch(this.Operacao){
+
+      case "+":
+        this.somar(this.PrimeiroNumero,this.SegundoNumero)
+        break
+      
+      case "-":
+          this.subtrair(this.PrimeiroNumero,this.SegundoNumero)
+          break
+
+      case  "*":
+        this.multiplicar(this.PrimeiroNumero,this.SegundoNumero)
+        break
+      case "/":
+        if(this.SegundoNumero === 0){
+          throw new Error("Divisão por 0 não existe")
+        }
+        this.dividir(this.PrimeiroNumero,this.SegundoNumero)
+        break
+      default:
+        console.log("Algo deu errado, refaça a operação."); 
+    }*/
+  }
+
+  somar(){
+    return this.PrimeiroNumero + this.SegundoNumero
+  }
+  subtrair(){
+  return this.PrimeiroNumero - this.SegundoNumero
+  }
+
+  multiplicar(){
+  return this.PrimeiroNumero * this.SegundoNumero
+  }
+  dividir(){
+  if(this.SegundoNumero === 0){
+    throw Error("Divisão inválida")
+  }
+  return Number((this.PrimeiroNumero / this.SegundoNumero).toFixed(3))
+  }
+}
+
+const calculadora = new Calculo(1,23)
+const somar = console.log(`Soma igual a ${calculadora.somar()}`)
+const subtracao = console.log(`Subtração igual a ${calculadora.subtrair()}`)
+const multiplicacao = console.log(`Multiplicação igual a ${calculadora.multiplicar()}`)
+const divisao = console.log(`Divisão igual a ${calculadora.dividir()}`)
