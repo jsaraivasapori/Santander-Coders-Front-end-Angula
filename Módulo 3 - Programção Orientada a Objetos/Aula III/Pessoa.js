483833f7357274c1dc015a96b1165b86549f7b4a
João Vitor <joaosaraivasapori@gmail.com>
2024-03-03 13:41:20 -0300

    Criação da classe Pessoa

---
+++ Módulo 3 - Programção Orientada a Objetos/Aula III/Pessoa.js
@@ -0,0 +1,42 @@
+ export class Pessoa{
+  Nome;
+  Idade;
+  Cpf;
+
+  constructor(nome = "",idade = "",cpf = ""){
+    if(this.#validarNome(nome) !== false) {
+      this.Nome = nome
+    }
+    if(this.#validarIdade(idade) !== false){
+      this.Idade = idade
+    } 
+    if(this.#validarCpf(cpf) !== false){
+      this.Cpf = cpf
+      return console.log("Cadastro rrealizado!");
+    }
+    return console.log("Refaça a operação, dados inválidos para ciração da Pessoa")
+
+    }
+  #validarNome(nome = ""){
+    if(nome ==="" || typeof nome !== "string"){
+      return false 
+  }
+    return true
+  }
+  #validarCpf(cpf = ""){
+    if(cpf ==="" || typeof cpf !== "string"){
+      return false 
+  }
+    return true
+  }
+  #validarIdade(idade = ""){
+    if(idade === "" || typeof idade !== "number"){
+      return false
+    }
+    return true
+  }
+ }
+
+
+const pessoa1 = new Pessoa("João Vitor",24,"kk")
+
