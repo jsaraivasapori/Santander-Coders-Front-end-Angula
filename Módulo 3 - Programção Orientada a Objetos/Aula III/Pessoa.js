efb37540825c0c38698dc4c8edb287962501f151
João Vitor <joaosaraivasapori@gmail.com>
2024-03-03 15:58:08 -0300

    Update método exibirDados

--- Módulo 3 - Programção Orientada a Objetos/Aula III/Pessoa.js
+++ Módulo 3 - Programção Orientada a Objetos/Aula III/Pessoa.js
@@ -1,22 +1,42 @@
  export class Pessoa{
-  Nome;
-  Idade;
-  Cpf;
+  #Nome;
+  #Idade;
+  #Cpf;
 
   constructor(nome = "",idade = "",cpf = ""){
     if(this.#validarNome(nome) !== false) {
-      this.Nome = nome
+      this.#Nome = nome
     }
     if(this.#validarIdade(idade) !== false){
-      this.Idade = idade
+      this.#Idade = idade
     } 
     if(this.#validarCpf(cpf) !== false){
-      this.Cpf = cpf
+      this.#Cpf = cpf
       return console.log("Cadastro realizado!");
     }
     return console.log("Refaça a operação, dados inválidos para ciração da Pessoa")
 
     }
+
+  get nome(){
+    return this.#Nome
+  }
+  
+  get idade(){
+    return this.#Idade
+  }
+
+  set idade(novaIdade){
+    if(novaIdade > this.#Idade && typeof novaIdade ==="number"){
+      return this.#Idade = novaIdade
+    }
+    return console.log("Entrada de dados errada. Operação abortada, revise os dados e tnte novamente.")
+  }
+
+  get cpf(){
+    return this.#Cpf
+  }
+
   #validarNome(nome = ""){
     if(nome ==="" || typeof nome !== "string"){
       return false 
@@ -36,11 +56,11 @@
     return true
   }
   exibirDados(){
-    console.log(this);
-  }
+    const dados = {Nome: this.nome,Idade: this.idade, Cpf: this.cpf }
+    return dados
+ }
  }
 
-
 const pessoa1 = new Pessoa("João Vitor",24,"kk")
-pessoa1.exibirDados()
+console.log(pessoa1.exibirDados())
 
