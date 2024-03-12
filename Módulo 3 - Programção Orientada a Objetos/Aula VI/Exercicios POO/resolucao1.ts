4982afe86b7a777fd816c5cf1a74ff827f09fa62
João <joaosaraivasapori@gmail.com>
2024-03-12 14:43:04 -0300

    Resolução exercício 1

---
+++ Módulo 3 - Programção Orientada a Objetos/Aula VI/Exercicios POO/resolucao1.ts
@@ -0,0 +1,30 @@
+class Produto{
+    private _nome : string
+    private _preco : number
+    private _quantidade : number
+    private _disponibilidade : string
+
+    constructor(nome:string,preco:number,quantidade:number){
+        this._nome = nome
+        this._preco = preco
+        this._quantidade = quantidade
+        this._disponibilidade = "Disponível"
+        
+    }
+
+    calcularValorTotal():void{
+        const valorTotal : number = this._preco * this._quantidade
+        return console.log(`Valor total em estoque: ${valorTotal}`)
+    }
+
+    verificarDisponibilidade() : string{
+        if(this._quantidade < 0){
+            return this._disponibilidade = "Indisponível"
+        }
+
+        return this._disponibilidade = "Disponível"
+    }
+
+    
+
+}
