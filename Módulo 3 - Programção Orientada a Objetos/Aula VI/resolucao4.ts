83547fe9bb35a7af5d5f8b6e99bc02c3f49166db
João Vitor <joaosaraivasapori@gmail.com>
2024-03-15 17:29:58 -0300

    Adcionado método adicionar e mudança para atributos e métodos estáticos na classe biblioteca

--- Módulo 3 - Programção Orientada a Objetos/Aula VI/resolucao4.ts
+++ Módulo 3 - Programção Orientada a Objetos/Aula VI/resolucao4.ts
@@ -10,13 +10,16 @@ class Livro{
   _anoPublicacao : number
 
   constructor(livro:Ilivro){
-    if(!this.validarString(livro.titulo)){
+    if(!this.validarStringTitulo(livro.titulo)){
       throw Error("Título inválido")
     }
+
     this._titulo = livro.titulo
-    if(!this.validarString(livro.autor)){
+
+    if(!this.validarStringAutor(livro.autor)){
       throw Error("Autor inválido")    
-    }   
+    } 
+
     this._autor =livro.autor
     this._anoPublicacao=livro.anoPublicacao
     
@@ -24,37 +27,85 @@ class Livro{
     
   }
 
-  private validarString(str : string): boolean{
-    const regex = /^[A-Za-z\u00C0-\u017F\s\p{P}]+$/u
+  private validarStringTitulo(srt:string):boolean{
+    if(srt === ""){
+      return false
+    }
+    return true
+  }
+
+  private validarStringAutor(str : string): boolean{
+    const regex = /^[A-Za-z\u00C0-\u017F\s-]+$/;
+
 
     return regex.test(str)
   }
+
 }
 
 class Biblioteca  {
   
-  private _listaLivros : Livro[]
+   static _listaLivros : Livro[] = []
 
-  constructor(){
-    
-    this._listaLivros = []
-  }
+   static adicionar(livro:Livro):boolean{
 
-  get listaLivros() : Livro[]{
-    return this._listaLivros
-  }
+    const verificacao : boolean = this._listaLivros.some((livroLista) => livroLista._titulo=== livro._titulo && livroLista._titulo && livroLista._autor === livro._autor && livroLista._anoPublicacao === livro._anoPublicacao)
 
-  adicionar(livro:Livro):boolean{
-    if(this._listaLivros.)
+    if(verificacao){    
+      console.error("Livro já existente no banco de dados")
+      return false
+    }
+  
     this._listaLivros.push(livro)
+    console.log(`Livro ${livro._titulo} adcionado com sucesso.`)
     return true
   }
   
 
 }
 
+const livro1: Ilivro = {
+  titulo: "1984",
+  autor: "George Orwell",
+  anoPublicacao: 1949
+};
 
-   
+const livro2: Ilivro = {
+  titulo: "O Senhor dos Anéis",
+  autor: "J.R.R. Tolkien",
+  anoPublicacao: 1954
+};
+
+const livro3: Ilivro = {
+  titulo: "Moby Dick",
+  autor: "Herman Melville",
+  anoPublicacao: 1851
+};
+
+const livro4: Ilivro = {
+  titulo: "Orgulho e Preconceito",
+  autor: "Jane Austen",
+  anoPublicacao: 1813
+};
+
+const livro5: Ilivro = {
+  titulo: "Cem Anos de Solidão",
+  autor: "Gabriel García Márquez",
+  anoPublicacao: 1967
+};
+
+const livro6: Ilivro = {
+  titulo: "Dom Quixote",
+  autor: "Miguel de Cervantes",
+  anoPublicacao: 1605
+};
+
+
+
+const livro = new Livro(livro1)
+Biblioteca.adicionar(livro)
+console.log(Biblioteca._listaLivros)
+
 
 
 
