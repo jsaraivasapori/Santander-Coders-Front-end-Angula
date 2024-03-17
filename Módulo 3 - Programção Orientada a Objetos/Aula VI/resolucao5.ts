8e849a72880ef958f3d47c09db1a11a93f48582b
João Vitor <joaosaraivasapori@gmail.com>
2024-03-16 23:26:13 -0300

    Resolução exercício 5

--- Módulo 3 - Programção Orientada a Objetos/Aula VI/resolucao5.ts
+++ Módulo 3 - Programção Orientada a Objetos/Aula VI/resolucao5.ts
@@ -1,5 +1,6 @@
 const moment = require('moment')
-interface Ipessoa{
+
+interface IPessoa{
   nome : string
   cpf : string
   dataNascimento : string
@@ -7,26 +8,27 @@ interface Ipessoa{
 
 class Pessoa {
 
-  private nome : string
-  private cpf : string
-  private dataNascimento : string
+  private _nome : string
+  private _cpf : string
+  private _dataNascimento : string
 
-  constructor(pessoa:Ipessoa){
+  constructor(pessoa:IPessoa){
 
     if(!this.validarNome(pessoa.nome)){
       throw Error("Nome inválido. Verifique e tente novamente")
     }
-    this.nome = pessoa.nome
+    this._nome = pessoa.nome
 
     if(!this.validarCpf(pessoa.cpf)){
       throw Error("Cpf inválido. Informe nesse padrão 000.000.000-00")
     }
-    this.cpf = pessoa.cpf
+
+    this._cpf = pessoa.cpf
 
     if(!this.validarDataNascimento(pessoa.dataNascimento)){
       throw Error("Data inválida. Verifique e tente novamente")
     }
-    this.dataNascimento = pessoa.dataNascimento
+    this._dataNascimento = pessoa.dataNascimento
   }
 
   private validarNome(nome:string) : boolean{
@@ -56,5 +58,39 @@ class Pessoa {
 
 }
 
+class Aluno extends Pessoa{
 
+  static matricula : number = 100
+  private numeroMatricula : number
+  private matriculaAtiva : boolean
 
+  constructor(aluno:IPessoa){
+    super(aluno)
+    Aluno.matricula ++
+    this.numeroMatricula = Aluno.matricula
+    this.matriculaAtiva = false
+    
+  }
+   alterarestadoMatricula(): void{
+    this.matriculaAtiva = !this.matriculaAtiva
+  }
+  situacao(pessoa:IPessoa):string{
+    return`
+       Aluno: ${pessoa.nome}  
+       CPF: ${pessoa.cpf}
+       Data de nascimento: ${pessoa.dataNascimento}
+       Matrícula: ${this.numeroMatricula}
+       Matrícula Ativa: ${this.matriculaAtiva}
+       `
+      
+    
+  }
+}
+      
+
+const pessoa1 : IPessoa= {nome: "João Vitor", cpf:"111.123.456-78", dataNascimento:"28/02/1999"}
+
+const aluno = new Aluno(pessoa1)
+
+aluno.alterarestadoMatricula()
+console.log(aluno.situacao(pessoa1))
