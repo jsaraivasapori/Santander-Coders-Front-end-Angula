8bf601f682538e48eee9b1497a7bd75a73b70e93
João <joaosaraivasapori@gmail.com>
2024-04-04 13:28:01 -0300

    Update exercicio lista de compras

--- Módulo 4 - Front End Dinâmico/listaCompras/index.js
+++ Módulo 4 - Front End Dinâmico/listaCompras/index.js
@@ -1,24 +1,27 @@
 let produtos = [];
-let id = 2;
+let id = 1;
 let limiteGasto = 0;
 
 function adcionarProduto() {
     const btnAdcionar = document.getElementById("add");
 
     btnAdcionar.addEventListener("click", () => {
-        const limitePreco = document.getElementById("limite").value.trim();
+        const limitePreco = parseFloat(
+            document.getElementById("limite").value.trim()
+        );
         const produto = document.getElementById("produto").value.trim();
         const valor = parseFloat(document.getElementById("valor").value);
-        limiteGasto = parseFloat(limitePreco);
-        produtos.push({
-            Id: id,
-            Produto: produto,
-            Valor: valor,
-        });
+
+        if (!verificarCamposVazios(limitePreco, produto, valor)) {
+            return console.log("Campo(s) vazio(s)");
+        }
+        limiteGasto = limitePreco;
+        armazenarListaArray(id, produto, valor);
         id++;
         listarProdutos();
-        riscarProdutoLista();
+
         indicarLimiteCompraEstourao(limitePreco);
+        selecionarProdutosDeletar();
     });
 }
 
@@ -29,13 +32,13 @@ function listarProdutos() {
     produtos.forEach((produto) => {
         li.innerHTML = `
         <input class='checkbox' type='checkbox' id='checkbox_produto_${produto.Id}'>
-        <p> ${produto.Produto} - R$ ${produto.Valor}</p>
+        <spam> ${produto.Produto}</spam>  <spam>R$ ${produto.Valor}</spam>
         `;
     });
     ul.append(li);
 }
 
-function riscarProdutoLista() {
+function selecionarProdutosDeletar() {
     const checkboxes = document.querySelectorAll("li .checkbox");
 
     checkboxes.forEach((box) => {
@@ -43,27 +46,70 @@ function riscarProdutoLista() {
             const estaSelecionado = box.checked;
             //parentElement pega todos os filhos diretos do elemento pai do checkbox
             //querrySelectoAll pega somente os spans
-            box.parentElement.querySelectorAll("p").forEach((el) => {
+            box.parentElement.querySelectorAll("spam").forEach((el) => {
                 el.style.textDecoration = estaSelecionado
                     ? "line-through"
                     : "none";
             });
         });
+
+        btnRemover(box);
     });
 }
 
 function indicarLimiteCompraEstourao(valorLimite) {
-    const spamSoma = document.querySelector("h3");
-    let somaProdutos = produtos.reduce(
-        (valor, produto) => (valor += produto.Valor),
-        0
-    );
-    console.log(somaProdutos);
+    const spamSoma = document.querySelector("span#soma");
+    let somaProdutos = produtos
+        .reduce((valor, produto) => (valor += produto.Valor), 0)
+        .toFixed(2);
+    mostrarSomaProdutos(somaProdutos);
     if (valorLimite < somaProdutos) {
         spamSoma.className = "checado";
         return;
     }
-    spamSoma.className = "none";
+    spamSoma.removeAttribute("class");
+}
+
+function mostrarSomaProdutos(total) {
+    somaParcial = document.getElementById("soma");
+    somaParcial.innerText = `Soma: ${total}`;
+}
+
+function verificarCamposVazios(valorLimite, produto, valorProduto) {
+    if (!valorLimite) {
+        alert("Valor limite inválido");
+        return false;
+    }
+    if (!produto) {
+        alert("Produto inválido");
+        return false;
+    }
+    if (!valorProduto) {
+        alert("Valor do produto inválido");
+        return false;
+    }
+
+    return true;
+}
+
+function armazenarListaArray(id, produto, valor) {
+    produtos.push({
+        Id: id,
+        Produto: produto,
+        Valor: valor,
+    });
+}
+
+function btnRemover(box) {
+    const btnRemover = document.getElementById("remover");
+
+    btnRemover.addEventListener("click", () => {
+        if (!produtos.length > 0) {
+            alert("Adicione algum produto primeiro para poder remover");
+            return console.error("Array vazio, impossível remover algo");
+        }
+        box.parentElement.remove();
+    });
 }
 
 adcionarProduto();
