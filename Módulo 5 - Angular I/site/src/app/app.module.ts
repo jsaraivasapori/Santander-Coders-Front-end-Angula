e3770293bba09e7d2169ab4de1b10139d0df2507
João <joaosaraivasapori@gmail.com>
2024-04-18 11:32:46 -0300

    Criação dos componentes about, address, contact e home

--- Módulo 5 - Angular I/site/src/app/app.module.ts
+++ Módulo 5 - Angular I/site/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AddressComponent } from './address/address.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AddressComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
