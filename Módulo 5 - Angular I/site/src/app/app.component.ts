ad3c2ca7158b37600a9a2b757c54f8d42dab9ec8
João <joaosaraivasapori@gmail.com>
2024-04-18 11:28:56 -0300

    Update

--- Módulo 5 - Angular I/site/src/app/app.component.ts
+++ Módulo 5 - Angular I/site/src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentPage: 'HOME' | 'ABOUT' | 'CONTACT' | 'ADDRESS' = 'HOME';
-
-  goToAbout(): void {
-    this.currentPage = 'ABOUT';
-  }
+  title = 'site';
 }
