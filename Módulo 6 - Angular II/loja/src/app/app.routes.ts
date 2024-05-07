a4540619cd3288ebe1ddf825a3cda7132227e59a
João <joaosaraivasapori@gmail.com>
2024-05-07 11:41:26 -0300

    Configuração inical do ambiente com Angular V17

---
+++ Módulo 6 - Angular II/loja/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { ProductsComponent } from './modules/products/products.component';
+
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: ProductsComponent
+    },
+    {
+
+    }
+];
