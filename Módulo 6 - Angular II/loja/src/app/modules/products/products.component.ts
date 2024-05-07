aeb368b180121917af6d96ff3707550ce2e9c211
João Vitor Saraiva Sapori <joaosaraivasapori@gmail.com>
2024-05-07 19:46:20 -0300

    update

--- Módulo 6 - Angular II/loja/src/app/modules/products/products.component.ts
+++ Módulo 6 - Angular II/loja/src/app/modules/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
