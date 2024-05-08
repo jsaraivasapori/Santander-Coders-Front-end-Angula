04f175a370b66e8979d399e8e66ca265e58f8a36
João <joaosaraivasapori@gmail.com>
2024-05-08 14:44:38 -0300

    Api sem autenticação

--- Módulo 6 - Angular II/products-api/modules/products/routes/products.routes.js
+++ Módulo 6 - Angular II/products-api/modules/products/routes/products.routes.js
@@ -1,13 +1,12 @@
 const { Router } = require("express");
-const verifyToken = require("../../../middlewares/verify-token");
 const productsController = require("../controllers/products.controller");
 
 const productsRouter = Router();
 
-productsRouter.get("/", verifyToken, productsController.findAll);
-productsRouter.get("/:id", verifyToken, productsController.findById);
-productsRouter.post("/", verifyToken, productsController.create);
-productsRouter.put("/:id", verifyToken, productsController.update);
-productsRouter.delete("/:id", verifyToken, productsController.delete);
+productsRouter.get("/", productsController.findAll);
+productsRouter.get("/:id", productsController.findById);
+productsRouter.post("/", productsController.create);
+productsRouter.put("/:id", productsController.update);
+productsRouter.delete("/:id", productsController.delete);
 
 module.exports = productsRouter;
