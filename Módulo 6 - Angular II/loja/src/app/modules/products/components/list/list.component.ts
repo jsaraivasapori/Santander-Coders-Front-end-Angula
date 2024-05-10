908520c96cc9c937a4c2d38b69da457df201e4ff
João Vitor <joaosaraivasapori@gmail.com>
2024-05-09 21:56:11 -0300

    update dele com modal de confirmação

--- Módulo 6 - Angular II/loja/src/app/modules/products/components/list/list.component.ts
+++ Módulo 6 - Angular II/loja/src/app/modules/products/components/list/list.component.ts
@@ -5,6 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { Subject, first, takeUntil } from 'rxjs';
 import { Product } from '../../models/product.model';
 import { ProductsService } from '../../service/products.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationModalComponent } from '../../../../commons/components/confirmation-modal/confirmation-modal.component';
 
 @Component({
   selector: 'app-list',
@@ -21,7 +23,7 @@ export class ListComponent implements OnInit , OnDestroy{
 
   apiUrl = "http://localhost:3000/products"
   
-  constructor(private productsService : ProductsService){}
+  constructor(private productsService : ProductsService, public dialog: MatDialog){}
 
 // Executando somente uma vez quando o componente e iniciado e pos receber todos os dados provenientes de inputs
   ngOnInit(): void {
@@ -60,9 +62,34 @@ export class ListComponent implements OnInit , OnDestroy{
     })
   }
 
+  openDialog(id: string): void {
+// modal salvo na variavel dialog    
+   const dialog = this.dialog.open(ConfirmationModalComponent, {
+      width: '250px',
+      disableClose: true,
+      data:{
+        id
+      }
+      
+    });
+// afterclose retorna um observable. afterclose é metodo de  MathDialogRef
+    dialog.afterClosed()
+    .pipe(first())
+    .subscribe((response)=>{
+      if(response){
+        this.onDelete(id)
+      }
+      
+      
+    })
+  }
+
+
   // boa pratica implementar isso, pois destroi tudo ao mudar de pagina, desescreve dos observables e protege contra danos de perfomace na aplicação
   ngOnDestroy(): void {
     this.ngUnsubscribe.next(true)
     this.ngUnsubscribe.complete()
   }
 }
+
+
