8858173022ae8a43645d98f81c3d98cd42f6322e
João <joaosaraivasapori@gmail.com>
2024-05-20 13:35:49 -0300

    Guard

--- Módulo 6 - Angular II/loja/src/app/modules/auth/services/auth.service.ts
+++ Módulo 6 - Angular II/loja/src/app/modules/auth/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 import { AuthenticatedUser } from '../models/authenticated-user.model';
 import { UserCredentials } from '../models/user-credentials.model';
 import { User } from '../models/user.model';
+import { Constants } from '../../../commons/constants/constants.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,8 @@ import { User } from '../models/user.model';
 export class AuthService {
   apiUrl = `${environment.API_URL}/auth`;
 
+
+  isLoggedIn = new BehaviorSubject<boolean>(false)//começa falso
   constructor(private http: HttpClient) {}
 
   register(user: User): Observable<void> {
@@ -20,5 +23,15 @@ export class AuthService {
 
   login(payload: UserCredentials): Observable<AuthenticatedUser> {
     return this.http.post<AuthenticatedUser>(`${this.apiUrl}/login`, payload);
+  }
+
+  checkAuthStatus() : Observable<boolean>{
+
+    const token = localStorage.getItem(Constants.TOKEN_KEY)
+    // toda vez que checar a função ceckAuthStatus, dispara um evento do tipo observable com valor boolean
+    this.isLoggedIn.next(!!token) // !! transforma em boolean
+    return this.isLoggedIn
+    
+
   }
 }
